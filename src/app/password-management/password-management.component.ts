af5a926275cc7bd57104d8c3a8964afedb671c20
willys_tjiawi <willys@huubap.com>
2020-03-02 12:04:28 +0800

    added password-management page

--- src/app/password-management/password-management.component.ts
+++ src/app/password-management/password-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {ContentModalComponent} from '../shared/content-modal.component';
 
 @Component({
   selector: 'password-management',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordManagementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+  onDetail() {
+    const modalRef = this.modalService.open(ContentModalComponent);
+    (modalRef.componentInstance as ContentModalComponent).data = 'dummy details';
+  }
 }
