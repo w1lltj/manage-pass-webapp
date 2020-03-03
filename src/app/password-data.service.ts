882f78cdd66a8ddb4dc7b21b5ab2e146ba89476a
willys_tjiawi <willys@huubap.com>
2020-03-04 01:58:39 +0800

    updated password-data service

--- src/app/password-data.service.ts
+++ src/app/password-data.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
+import {PasswordData} from './password-data.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +13,28 @@ export class PasswordDataService {
 
   onGetAllDatas(): Observable<any> {
     const token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    });
-
-    console.log('isAuth_token..... ', token, headers);
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this._httpClient.get('http://localhost:3001/password-data/list', {headers: headers})
       .pipe(
+        map(
+          (res: any) => res.passwordDatas
+        ),
         catchError((err: HttpErrorResponse) => {
-          console.log('isAuth_err..... ', err);
+          console.log('getAllDatas_err..... ', err);
+          return throwError(err);
+        })
+      );
+  }
+
+  onAddData(passwordData: PasswordData): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this._httpClient.post('http://localhost:3001/password-data/add', passwordData, {headers: headers})
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          console.log('addData_err..... ', err);
           return throwError(err);
         })
       );
